e1d950015187c31fc973b95e4210d6488f492851
jaeyeong04 <jeayoung05050@naver.com>
2025-10-14 16:09:51 +0900

    refactor: [Unit.tsx] code refactoring by using styled-component

--- src/components/atoms/Unit.tsx
+++ src/components/atoms/Unit.tsx
@@ -1,5 +1,6 @@
 import { UnitVariant } from "../../constants/enum";
 import theme from "../../constants/theme";
+import styled from "styled-components";
 
 //variant별 색상 설정
 const colorMap: { [key in UnitVariant]: string } = {
@@ -8,20 +9,22 @@ const colorMap: { [key in UnitVariant]: string } = {
   [UnitVariant.COIN]: theme.gold,
 };
 
+const StyledUnit = styled.span<{ variant: UnitVariant }>`
+  width: 20px;
+  height: 20px;
+  display: inline-block;
+  background-color: ${(props) => colorMap[props.variant]};
+  border-radius: ${(props) =>
+    props.variant === UnitVariant.PLAYER ? "0%" : "50%"};
+  border: 3px solid black;
+  text-align: center;
+`;
+
 //Unit은 게임에 필요한 '말'을 의미함.
 //variant는 '말'의 종류를 의미함. 예: player, enemy, coin 등
 //variant의 크기는 모두 같음. 단, 색은 다름.
 //variant는 enum으로 관리함.
 //player만 네모, 나머지는 동그라미로 표시함.
 export const Unit = ({ variant }: { variant: UnitVariant }) => {
-  const style = {
-    width: "20px",
-    height: "20px",
-    display: "inline-block",
-    backgroundColor: colorMap[variant],
-    borderRadius: variant === UnitVariant.PLAYER ? "0%" : "50%",
-    border: "3px solid black",
-    textAlign: "center" as const,
-  };
-  return <span style={style}></span>;
+  return <StyledUnit variant={variant}></StyledUnit>;
 };
