6d1f1ff333bc4edc44906cfffa62403a9325175f
jaeyeong04 <jeayoung05050@naver.com>
2025-10-14 16:05:35 +0900

    feat: [Board.tsx] UI completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Board } from "./components/atoms/Board";
 import { Unit } from "./components/atoms/Unit";
 import { UnitVariant } from "./constants/enum";
 
 function App() {
   return (
     <div className="App">
+      <Board />
       <Unit variant={UnitVariant.PLAYER} />
       <Unit variant={UnitVariant.ENEMY} />
       <Unit variant={UnitVariant.COIN} />
---
+++ src/components/atoms/Board.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const BoardContainer = styled.div`
+  width: 600px;
+  height: 600px;
+  border: 3px solid black;
+`;
+
+export const Board = () => {
+  return <BoardContainer></BoardContainer>;
+};
