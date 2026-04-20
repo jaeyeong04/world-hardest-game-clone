c275df4a61a52e91cf4df34a4c9a2d8a167300c6
jaeyeong04 <jeayoung05050@naver.com>
2026-04-20 21:36:13 +0900

    feat: [GameRecord] displays elapsed time

--- src/components/molecules/GameRecord.tsx
+++ src/components/molecules/GameRecord.tsx
@@ -1,6 +1,8 @@
 import { Text } from "../atoms/Text";
 import { TextUsage } from "../../constants/enum";
 import styled from "styled-components";
+import useTimer from "../../hooks/useTimer";
+import { useEffect } from "react";
 
 const GameRecordContainer = styled.div`
   display: flex;
@@ -10,12 +12,21 @@ const GameRecordContainer = styled.div`
   justify-content: space-between;
 `;
 
+//time을 화면에 표시하기 위해 format하는 함수
+//time은 초 단위로 저장되어 있으므로, ss.ms 형식으로 변환하여 표시
+const formatTime = (time: number) => {
+  const seconds = Math.floor(time);
+  const milliseconds = Math.floor((time - seconds) * 100);
+  return `${seconds}:${milliseconds.toString().padStart(2, "0")}`;
+};
+
 export const GameRecord = () => {
+  const time = useTimer();
   return (
     <GameRecordContainer>
       <Text usage={TextUsage.LIVES}>Lives: 3</Text>
       <Text usage={TextUsage.SCORE}>Score: 100</Text>
-      <Text usage={TextUsage.TIME}>Time: 00:45</Text>
+      <Text usage={TextUsage.TIME}>Time: {formatTime(time)}</Text>
     </GameRecordContainer>
   );
 };
