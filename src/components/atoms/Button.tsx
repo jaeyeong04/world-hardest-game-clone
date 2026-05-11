ad26505adf032914bb5ca03617b3cb5626dccd8b
jaeyeong04 <jeayoung05050@naver.com>
2026-05-11 19:59:59 +0900

    chore: [Button] 컴포넌트의 UI 변경

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,8 +1,25 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: 1px solid black;
+  background-color: white;
+  font-size: 30px;
+  padding: 10px 20px;
+  border-radius: 8px;
+  font-weight: bold;
+`;
+
 /**
  * @params text - button text
  * @returns button element w/ text
  */
 
-export function Button() {
-  return <button>Click Me</button>;
+export function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
 }
